187c0ab1959a908ee246fa114743a17b010fb630
Akuma2302 <asyraaf2302@gmail.com>
2026-07-02 13:28:46 +0800

    header modify

--- frontend/src/layout/header.js
+++ frontend/src/layout/header.js
@@ -1,14 +1,3 @@
-export function populateSalespersonFilter(allEntries) {
-  const select = document.getElementById('sp-filter');
-  const current = select.value;
-  const names = [...new Set(allEntries.map((e) => e.salesperson))].sort();
-
-  select.innerHTML = '<option value="all">All Salespersons</option>' +
-    names.map((n) => `<option value="${n}">${n}</option>`).join('');
-
-  if (names.includes(current)) select.value = current;
-}
-
 export function setLastUpdated(allEntries) {
   const el = document.getElementById('last-updated');
   const spCount = new Set(allEntries.map((e) => e.salesperson)).size;
